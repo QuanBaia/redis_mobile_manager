7613c82282df4fc1a777171652f24aee7dda9f67
Quan Bai <2743611736@qq.com>
2024-04-19 22:19:17 +0800

    add i18n support

---
+++ lib/common/i18n/content.dart
@@ -0,0 +1,18 @@
+///
+/// i18n content
+///
+class Content {
+
+  // home page
+  //标题
+  static const String homePageTitle = "home_page_title";
+  //空白页描述
+  static const String homePageIsEmptyText = "home_page_description";
+
+
+
+
+
+
+
+}
---
+++ lib/common/i18n/messages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'package:redis_mobile_manager/common/i18n/content.dart';
+
+class Messages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+    "zh_CN" : {
+      Content.homePageTitle : "主机",
+      Content.homePageIsEmptyText : "请先添加 redis 连接",
+    },
+    "zh_HK":{
+      Content.homePageTitle : "主機",
+      Content.homePageIsEmptyText : "請先新增 redis 連接",
+    },
+    "en_US":{
+      Content.homePageTitle : "HOST",
+      Content.homePageIsEmptyText : "Please add redis connection first",
+    },
+
+    "ja_JP":{
+      Content.homePageTitle : "ホスト",
+      Content.homePageIsEmptyText : "まず、redis 接続を追加してください",
+    },
+
+  };
+
+}
