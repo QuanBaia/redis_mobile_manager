3d1ce91ae335e9d58ea99dd713e263b1f7a938dc
Quan Bai <2743611736@qq.com>
2024-04-20 04:06:34 +0800

    add home_page => tooltips message

--- lib/common/i18n/content.dart
+++ lib/common/i18n/content.dart
@@ -8,7 +8,10 @@ class Content {
   static const String homePageTitle = "home_page_title";
   //空白页描述
   static const String homePageIsEmptyText = "home_page_description";
-
+  //添加按钮提示
+  static const String homePageAddButtonHint = "home_page_add_button_hint";
+  //菜单栏
+  static const String homePageMenu = "home_page_menu";
 
 
 
--- lib/common/i18n/messages.dart
+++ lib/common/i18n/messages.dart
@@ -8,18 +8,27 @@ class Messages extends Translations {
     "zh_CN" : {
       Content.homePageTitle : "主机",
       Content.homePageIsEmptyText : "请先添加 redis 连接",
+      Content.homePageAddButtonHint : "添加一个连接",
+      Content.homePageMenu: "打开菜单",
+
     },
     "zh_HK":{
       Content.homePageTitle : "主機",
       Content.homePageIsEmptyText : "請先新增 redis 連接",
+      Content.homePageAddButtonHint : "新增一個連接",
+      Content.homePageMenu: "開啟菜單",
     },
     "en_US":{
       Content.homePageTitle : "HOST",
       Content.homePageIsEmptyText : "Please add redis connection first",
+      Content.homePageAddButtonHint : "Add a connection",
+      Content.homePageMenu: "Open menu",
     },
     "ja_JP":{
       Content.homePageTitle : "ホスト",
       Content.homePageIsEmptyText : "まず、redis 接続を追加してください",
+      Content.homePageAddButtonHint : "接続を追加",
+      Content.homePageMenu: "メニューを開く",
     },
 
   };
