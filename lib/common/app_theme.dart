a7650cdaf6e262103ef9b0826759fb272ebf5b41
Quan Bai <2743611736@qq.com>
2024-04-20 00:23:56 +0800

    simple app theme config

--- lib/common/app_theme.dart
+++ lib/common/app_theme.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
+// app 主题管理
 class AppTheme {
 
   // main theme
   static ThemeData mainTheme = ThemeData(
+    //font theme
     fontFamily: 'HuaWeiSans',
+
+    //app bar theme
+    appBarTheme: const AppBarTheme(
+      color: Color(0xfffafbfd),
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    //page theme
+    scaffoldBackgroundColor: const Color(0xffedf1f2),
 
   );
 
   static ThemeData mainDarkTheme = ThemeData(
 
+    //font theme
     fontFamily: 'HuaWeiSans',
+    //app bar theme
+    appBarTheme: const AppBarTheme(
+      color: Color(0xff292a3c),
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    //page theme
+    scaffoldBackgroundColor: const Color(0xff141527),
+    //text theme
 
   );
 
