fb6b1c6359260a70fb373ebce63ce4546b494475
Quan Bai <2743611736@qq.com>
2024-04-20 13:50:53 +0800

    add => setting_view

---
+++ lib/pages/setting/setting_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Center(
+      child: Container(
+        child: Text("Setting Page"),
+      ),
+    );
+  }
+}
